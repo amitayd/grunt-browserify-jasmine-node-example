3ad692026f8da2a7e69b522e4e8739b2f346f3d5
Amitay Dobo <amitayd@gmail.com>
2013-11-02 16:49:06 +0200

    removed newline in end of hello.txt

--- test/spec/node/HelloFileSystemSpec.js
+++ test/spec/node/HelloFileSystemSpec.js
@@ -4,6 +4,6 @@ var HelloFileSystem = require('../../../src/node/HelloFileSystem');
 
 describe("HelloWorld", function() {
     it("hello() should say hello from file when called", function() {
-        expect(HelloFileSystem.hello()).toEqual("hello\n");
+        expect(HelloFileSystem.hello()).toEqual("hello");
     });
 });
